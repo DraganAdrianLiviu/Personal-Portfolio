d933f909bd8a3c8b76e4d4630853ef61d6922fe9
DraganAdrianLiviu <dragan.adrian.liviu@gmail.com>
2023-12-21 11:57:23 +0200

    Day/Night mode

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Typing Animation */
+var typed = new Typed(".typing", {
+    strings:["Mechanical Design Engineer", "Lead Designer", "Budding Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
